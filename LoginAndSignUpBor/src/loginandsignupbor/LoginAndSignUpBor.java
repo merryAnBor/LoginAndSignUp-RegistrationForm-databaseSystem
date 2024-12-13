35fde8e38a20bd8fc98f1b402b80b2afafd84df9
merryAnBor <borresmerryan5@gmail.com>
2024-12-13 18:07:24 +0800

    Add files via upload

---
+++ LoginAndSignUpBor/src/loginandsignupbor/LoginAndSignUpBor.java
@@ -0,0 +1,14 @@
+ 
+package loginandsignupbor;
+
+ 
+public class LoginAndSignUpBor {
+    public static void main(String[] args) {
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
